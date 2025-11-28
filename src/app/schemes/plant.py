5ccc3bb01c4a6a244feeb5caea6627cb2d299775
Vasilii Stakrotckii <142693897+inferno681@users.noreply.github.com>
2025-11-28 12:20:44 +0100

    plants status

--- src/app/schemes/plant.py
+++ src/app/schemes/plant.py
@@ -1,35 +1,8 @@
-from datetime import date
+from datetime import date, timedelta
 
-from pydantic import BaseModel, ConfigDict, field_validator
+from pydantic import BaseModel, ConfigDict, computed_field, field_validator
 
 from app.models.plant import FertilizingPeriod, WateringPeriod
-
-
-class PlantReadScheme(BaseModel):
-
-    id: str
-    name: str
-    scientific_name: str | None = None
-    description: str | None = None
-    image: str | None = None
-    image_url: str | None = None
-
-    warm_period: WateringPeriod | None = None
-    cold_period: WateringPeriod | None = None
-    fertilizing: FertilizingPeriod | None = None
-
-    last_watered_at: date | None = None
-    last_fertilized_at: date | None = None
-
-    next_watering_at: date | None = None
-    next_fertilizing_at: date | None = None
-
-    model_config = ConfigDict(from_attributes=True)
-
-    @field_validator('id', mode='before')
-    @classmethod
-    def cast_id(cls, id):
-        return str(id)
 
 
 class PlantReadSchemeShort(BaseModel):
@@ -50,3 +23,38 @@ class PlantReadSchemeShort(BaseModel):
     @classmethod
     def cast_id(cls, id):
         return str(id)
+
+    @computed_field
+    @property
+    def status(self) -> str:
+        today = date.today()
+        if self.next_watering_at:
+            if self.next_watering_at < today:
+                return 'needs_watering'
+            elif self.next_watering_at == today + timedelta(days=1):
+                return 'watering_tomorrow'
+            else:
+                return 'healthy'
+        return 'healthy'
+
+
+class PlantReadScheme(PlantReadSchemeShort):
+
+    id: str
+    name: str
+    scientific_name: str | None = None
+    description: str | None = None
+    image: str | None = None
+    image_url: str | None = None
+
+    warm_period: WateringPeriod | None = None
+    cold_period: WateringPeriod | None = None
+    fertilizing: FertilizingPeriod | None = None
+
+    last_watered_at: date | None = None
+    last_fertilized_at: date | None = None
+
+    next_watering_at: date | None = None
+    next_fertilizing_at: date | None = None
+
+    model_config = ConfigDict(from_attributes=True)
