efc4cbb51ea0d2f5964dae6a42847d2c919681d2
Vasilii Stakrotckii <142693897+inferno681@users.noreply.github.com>
2026-01-23 09:56:41 +0100

    di

--- src/app/dependencies/db.py
+++ src/app/dependencies/db.py
@@ -5,7 +5,7 @@ from fastapi import Depends
 from app.db import DbHelper, get_db_helper
 
 
-async def get_session(db_helper: Annotated[DbHelper, get_db_helper]):
+async def get_session(db_helper: Annotated[DbHelper, Depends(get_db_helper)]):
     """Get db session dependency."""
     return db_helper.transaction()
 
